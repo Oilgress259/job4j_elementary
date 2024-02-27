b3e26dd81476eb982de5ad4060694ab22bd983fb
Nikita Eltsov <wendris202@mail.ru>
2024-02-27 09:47:24 +0300

    4.1. Операторы ветвлений [#3611]

---
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+public class AlertDivByZero {
+
+    public static void checkNumber(int number) {
+        if (number == 0) {
+            System.out.println("Could not div by 0.");
+        } else if (number < 0) {
+            System.out.println("This is negative number");
+        }
+    }
+
+    public static void main(String[]args) {
+            AlertDivByZero.checkNumber(-2);
+            AlertDivByZero.checkNumber(0);
+            AlertDivByZero.checkNumber(0);
+    }
+}
+
+
+
